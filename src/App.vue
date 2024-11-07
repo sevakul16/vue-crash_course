92765e6dd510ae3965114966e7d05a36fc44428a
sevakul16 <sevakul16@gmail.com>
2024-11-07 17:25:20 +0200

    Added add and delete

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,23 @@
 import { ref } from "vue";
 const name = ref("John Doe");
 const status = ref("offline");
-const tasks = ref([
-  { id: 1, title: "Task 1" },
-  { id: 2, title: "Task 2" },
-  { id: 3, title: "Task 3" },
-]);
+const tasks = ref(["Task 1", "Task 2", "Task 3"]);
 const link = "https://www.google.com";
+const newTask = ref("");
 
 const toggleStatus = () => {
   status.value = status.value === "online" ? "offline" : "online";
+};
+
+const addTask = () => {
+  if (newTask.value) {
+    tasks.value.push(newTask.value);
+    newTask.value = "";
+  }
+};
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
 };
 </script>
 
@@ -19,9 +27,17 @@ const toggleStatus = () => {
   <h2 v-if="status === 'online'">User is online</h2>
   <h2 v-else-if="status === 'offline'">User is offline</h2>
   <h2 v-else>User status is unknown</h2>
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" v-model="newTask" />
+    <button type="submit" @click="addTask">Add</button>
+  </form>
+
   <ul>
-    <li v-for="task in tasks" :key="task.id">
-      {{ task.title }}
+    <li v-for="(task, index) in tasks" :key="task">
+      <span> {{ task }} </span>
+      <button @click="deleteTask(index)">Delete</button>
     </li>
   </ul>
   <a :href="link">Google</a>
