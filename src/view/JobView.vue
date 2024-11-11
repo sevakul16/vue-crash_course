4e983c2d1f4f911ab31aafa967fb3d87998a1888
sevakul16 <sevakul16@gmail.com>
2024-11-11 16:08:12 +0200

    Delete job

--- src/view/JobView.vue
+++ src/view/JobView.vue
@@ -2,10 +2,15 @@
 import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 import axios from "axios";
 import { onMounted, reactive } from "vue";
-import { useRoute, RouterLink } from "vue-router";
+import { useRoute, RouterLink, useRouter } from "vue-router";
 import BackButton from "@/components/BackButton.vue";
+import { useToast } from "vue-toastification";
 
 const route = useRoute();
+
+const router = useRouter();
+
+const toast = useToast();
 
 const jobId = route.params.id;
 
@@ -13,6 +18,19 @@ const state = reactive({
   job: {},
   isLoading: true,
 });
+
+const deleteJob = async () => {
+  try {
+    const confirm = window.confirm("Are you sure you want to delete this job?");
+    if (!confirm) return;
+    await axios.delete(`/api/jobs/${jobId}`);
+    toast.success("Job deleted successfully");
+    router.push("/jobs");
+  } catch (error) {
+    console.error(error);
+    toast.error("An error occurred. Please try again.");
+  }
+};
 
 onMounted(async () => {
   try {
@@ -97,6 +115,7 @@ onMounted(async () => {
               >Edit Job</RouterLink
             >
             <button
+              @click="deleteJob"
               class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
             >
               Delete Job
